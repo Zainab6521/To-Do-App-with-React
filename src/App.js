591a0088ef90bc876e274f3160dc709b97aab814
Zainab6521 <zk.652162@gmail.com>
2021-05-22 12:45:28 +0500

    id key added to expenseItem

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import { useState } from "react";
+import react, { useState } from "react";
 import "./App.css";
 import Expenses from "./Components/Expenses/Expenses";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Learn React.",
+    amount: 261.67,
+    date: new Date(2021, 10, 8),
+  },
+  {
+    id: "e2",
+    title: "Car Insurance",
+    amount: 261.67,
+    date: new Date(2021, 5, 18),
+  },
+  {
+    id: "e3",
+    title: "pick Abbiiiii",
+    amount: 100,
+    date: new Date(2021, 9, 19),
+  },
+  {
+    id: "e4",
+    title: "New MObile",
+    amount: 450000,
+    date: new Date(2022, 4, 22),
+  },
+];
+
 const App = () => {
-  const DUMMY_EXPENSES = [
-    {
-      id: "e1",
-      title: "Learn React.",
-      amount: 261.67,
-      date: new Date(2021, 10, 8),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance",
-      amount: 261.67,
-      date: new Date(2021, 5, 18),
-    },
-    {
-      id: "e3",
-      title: "pick Abbiiiii",
-      amount: 100,
-      date: new Date(2021, 9, 19),
-    },
-    {
-      id: "e4",
-      title: "New MObile",
-      amount: 450000,
-      date: new Date(2022, 4, 22),
-    },
-  ];
-  const App = () => {
-    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-    const addExpenseHandler = (expenses) => {
-      setExpenses((prevExpenses) => {
-        return [expense, ...prevExpenses];
-      });
-    };
+  const addExpenseHandler = (expenses) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
-  const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
-  };
 
-  return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
-    </div>
-  );
+const addExpenseHandler = (expense) => {
+  console.log("In App.js");
+  console.log(expense);
 };
 
+return (
+  <div>
+    <NewExpense onAddExpense={addExpenseHandler} />
+    <Expenses items={expenses} />
+  </div>
+);
+};
 export default App;
